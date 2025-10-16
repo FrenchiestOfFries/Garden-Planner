892ee3d6da6a169a4ed5e3c91e21f437f6053a85
Kyle Korando <kakxcd@umsystem.edu>
2025-10-16 18:30:40 -0500

    main tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css"; // Tailwind entry if you have it
 
-const router = createBrowserRouter([
-    { path: '/', element: <App /> },
-    // { path: '/plants/:id', element: <PlantDetail /> },
-])
-
-createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+  </React.StrictMode>
+);
